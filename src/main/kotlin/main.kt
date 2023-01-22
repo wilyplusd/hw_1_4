4ad3a90c4c0cad00c616e5e3d75b18d07d0447f9
Irina Derevyanko <wily@list.ru>
2023-01-22 17:34:08 +0300

    test passed

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,60 @@
+enum class KindPay {
+    MASTERCARD, MAESTRO, VKPAY, VISA, MIR
+}
+
+fun takeTransferFee(
+    amount: Double,
+    previousTransfer: Double,
+    kindPayTransfer: KindPay = KindPay.VKPAY,
+): String {
+    val percentTransfer: Double
+    val minTransfer: Double
+    val maxTransfer: Double
+    val aboveTransfer: Double
+    var transferFee: Double
+
+    when (kindPayTransfer) {
+        KindPay.VKPAY -> {
+            percentTransfer = 0.00;
+            minTransfer = 0.00;
+            maxTransfer = 0.00;
+            aboveTransfer = 0.00
+        }
+
+        KindPay.MAESTRO, KindPay.MASTERCARD -> {
+            percentTransfer = 0.006;
+            minTransfer = 0.00;
+            maxTransfer = 75000.00
+            aboveTransfer = 20.00
+        }
+
+        KindPay.VISA, KindPay.MIR -> {
+            percentTransfer = 0.0075;
+            minTransfer = 35.00;
+            maxTransfer = 0.00
+            aboveTransfer = 0.00
+        }
+    }
+
+    transferFee = amount * percentTransfer + aboveTransfer
+
+    if (kindPayTransfer == KindPay.MIR || kindPayTransfer == KindPay.VISA) {
+        if (transferFee < minTransfer) {
+            transferFee = minTransfer
+        }
+    }
+
+    if (kindPayTransfer == KindPay.MAESTRO || kindPayTransfer == KindPay.MASTERCARD) {
+        if (previousTransfer < maxTransfer) {
+            transferFee = 0.00
+        }
+    }
+
+    val transferAmount = amount - transferFee
+    return "Ваша комиссия за перевод: $transferFee сумма перевода $transferAmount"
+}
+
+fun main() {
+    print(takeTransferFee(80000.00,  80000.00, KindPay.MIR))
+
+}
---
+++ src/test/kotlin/ru_netology/MainKtTest.kt
@@ -0,0 +1,35 @@
+package ru_netology
+
+import org.junit.jupiter.api.Test
+import takeTransferFee
+import kotlin.test.assertEquals
+
+class MainKtTest {
+
+    @Test
+    fun takeTransferFee() {
+        val test1 = takeTransferFee(100.00,  74000.00, KindPay.VISA)
+        val test2 = takeTransferFee(76000.00,  80000.00, KindPay.MAESTRO)
+        val test3 = takeTransferFee(2000.00,  74000.00, KindPay.MASTERCARD)
+        val test4 = takeTransferFee(80000.00,  80000.00, KindPay.MIR)
+        val test5 = takeTransferFee(100.00,  74000.00, KindPay.VKPAY)
+        val test6 = takeTransferFee(72000.00,  76000.00, KindPay.MASTERCARD)
+        val test7 = takeTransferFee(100.00, 74000.00)
+        val reference1 = "Ваша комиссия за перевод: 35.0 сумма перевода 65.0"
+        val reference2 = "Ваша комиссия за перевод: 476.0 сумма перевода 75524.0"
+        val reference3 = "Ваша комиссия за перевод: 0.0 сумма перевода 2000.0"
+        val reference4 = "Ваша комиссия за перевод: 600.0 сумма перевода 79400.0"
+        val reference5 = "Ваша комиссия за перевод: 0.0 сумма перевода 100.0"
+        val reference6 = "Ваша комиссия за перевод: 452.0 сумма перевода 71548.0"
+        val reference7 = "Ваша комиссия за перевод: 0.0 сумма перевода 100.0"
+
+        assertEquals(expected = reference1, actual = test1)
+        assertEquals(expected = reference2, actual = test2)
+        assertEquals(expected = reference3, actual = test3)
+        assertEquals(expected = reference4, actual = test4)
+        assertEquals(expected = reference5, actual = test5)
+        assertEquals(expected = reference6, actual = test6)
+        assertEquals(expected = reference7, actual = test7)
+
+    }
+}
